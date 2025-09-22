7902011844dd668dc776f21e5403ef854bed5632
jamesfbmd-dev <jamesfbmd@gmail.com>
2025-09-22 15:53:29 +1000

    Test Push

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -4,11 +4,13 @@
 // click the gear icon -> Project settings, and under the "General" tab,
 // scroll down to "Your apps" and select the web app. You will find
 // the firebaseConfig object there.
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+// export const firebaseConfig = {
+//   apiKey: "YOUR_API_KEY",
+//   authDomain: "YOUR_AUTH_DOMAIN",
+//   projectId: "YOUR_PROJECT_ID",
+//   storageBucket: "YOUR_STORAGE_BUCKET",
+//   messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+//   appId: "YOUR_APP_ID"
+// };
+
+//test
