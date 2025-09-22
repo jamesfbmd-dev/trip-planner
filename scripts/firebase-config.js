1d1e2cc8ded6bdfcfac69168807ce15d979a9d11
jamesfbmd-dev <jamesfbmd@gmail.com>
2025-09-22 15:10:41 +1000

    Update firebase-config.js

---
+++ scripts/firebase-config.js
@@ -0,0 +1,14 @@
+// TODO: Replace with your project's Firebase credentials.
+// This object will be used to initialize Firebase in script.js.
+// To get this configuration, go to your Firebase project console,
+// click the gear icon -> Project settings, and under the "General" tab,
+// scroll down to "Your apps" and select the web app. You will find
+// the firebaseConfig object there.
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
